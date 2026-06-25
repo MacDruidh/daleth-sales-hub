879c30007566121cf26b2b104950b5a063bfe696
Sergio Paulo Andrade <sergiopauloandrade@MacBook-Air-3.local>
2026-06-25 15:28:54 -0300

    Forca favicon da Daleth no Safari

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'daleth-sales-hub-v3';
-const APP_SHELL = ['/', '/manifest.webmanifest', '/daleth-star.png'];
+const CACHE_NAME = 'daleth-sales-hub-v4';
+const APP_SHELL = ['/', '/manifest.webmanifest', '/daleth-star.png', '/pwa-icon.svg', '/favicon.ico'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
